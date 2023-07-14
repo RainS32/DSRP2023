548cc07b290c80c08b3355b928376c7f77c5851a
RainS32 <arin.shukla@gmail.com>
2023-07-14 13:09:54 -0700

    Pushing more files

--- Labs/lab7-hypothesis -testing.R
+++ Labs/lab7-hypothesis -testing.R
@@ -4,13 +4,45 @@ library(dplyr)
 library(ggplot2)
 
 getwd()
-nba <- read.csv("DSRP2023/Datasets/championsdata.csv")
+nba <- read.csv("Datasets/championsdata.csv")
 View(nba)
 
+## T-test ####
+
 ## Compare the total rebounds per game between the lakers and the celtics
-## null hypothesis: average total rebounds per game of between the lakers and the celtics is the same
-## alternative hypothesis: average total rebounds per game of between the lakers and the celtics is different
+## null hypothesis: average total rebounds per game between the lakers and the celtics is the same
+## alternative hypothesis: average total rebounds per game between the lakers and the celtics is different
+
 lakers <- nba |> filter(Team == "Lakers")
 celtics <- nba |> filter(Team == "Celtics")
 
 t.test(lakers$TRB, celtics$TRB, paired = F, alternative = "two.sided")
+
+## ANOVA ####
+
+## Compare the total assists per game between the lakers, bulls, and spurs
+## null hypothesis: average total assists per game of between the lakers, bulls, and the spurs is the same
+## alternative hypothesis: average total rebounds per game of between the lakers, bulls, and the spurs is different
+
+top3teams <- nba |>
+  summarize(.by = Team,
+            count = sum(!is.na(Team))) |>
+  slice_max(count, n = 3)
+
+top3teams 
+
+nba_top3teams <- nba |>
+  filter(Team %in% top3teams$Team)
+
+a <- aov(AST ~ Team, data = nba_top3teams)
+summary(a)
+TukeyHSD(a)
+
+## Chi-Square ####
+
+## Compare whether points scored and assists are related
+##Null Hypothesis: Points scored is independent from assists
+##Alternate Hypothesis: Points scored is associated with assists (or not independent)
+
+con_table <- table(nba$AT, nba$PTS)
+results <- chisq.test(con_table)
--- Notes/Day 6 Hypothesis testing.R
+++ Notes/Day 6 Hypothesis testing.R
@@ -1,3 +1,6 @@
+library(dplyr)
+library(ggplot2)
+
 ## Compare the mass of male and female human star wars characters?
 ## null hypothesis: average mass of male and female star wars characters is the same
 ## alternative hypothesis: average mass of male and female star wars characters is different
@@ -9,3 +12,91 @@ females <- swHumans |> filter(sex == "female")
 t.test(males$mass, females$mass, paired = F, alternative = "two.sided")
 # p value is 0.06
 # not significant, failed to reject null hypothesis
+
+## ANOVA ####
+iris
+
+anova_results <- aov(Sepal.Width ~ Species, data = iris)
+
+## Are there any groups different from each other
+summary(anova_results)
+
+## Which ones?
+TukeyHSD(anova_results)
+
+## Is there a significant difference in the mean petal lengths or petal widths by species?
+anova_results <- aov(Petal.Width ~ Species, data = iris)
+summary(anova_results)
+TukeyHSD(anova_results)
+
+### Starwars
+head(starwars)
+unique(starwars$species)
+
+## Which 5 species are the most common?
+top3species <- starwars |>
+  summarize(.by = species,
+            count = sum(!is.na(species))) |>
+  slice_max(count, n = 3)
+
+top3species
+
+starwars_top3species <- starwars |>
+  filter(species %in% top3species$species)
+
+## Is there a significant difference in the mass of each of the top 3 species?
+a <- aov(mass ~ species, data = starwars_top3species)
+summary(a)
+TukeyHSD(a)
+
+a <- aov(height ~ species, data = starwars_top3species)
+summary(a)
+TukeyHSD(a)
+
+## Chi-Squared ####
+starwars_clean <- starwars |>
+  filter(!is.na(species),
+         !is.na(homeworld))
+
+t <- table(starwars$species, starwars$homeworld)
+chisq.test(t) # not enough data, needs at least 10 values each column
+
+mpg
+table(mpg$manufacturer, mpg$class)
+table(mpg$cyl, mpg$displ)
+
+## How do we get a contingency table of year and drv?
+t <- table(mpg$year, mpg$drv)
+
+chisq_result <- chisq.test(t)
+chisq_result
+chisq_result$p.value
+chisq_result$residuals
+
+install.packages("corrplot")
+library(corrplot)
+
+corrplot(chisq_result$residuals)
+
+##chi-squared full anaylsis
+heroes <- read.csv("Datasets/heroes_information.csv")
+head(heroes)
+
+## clean data
+heroes_clean <- heroes |>
+  filter(Alignment != "-",
+         Gender != "-")
+
+## plot the counts of alignment and gender
+ggplot(heroes_clean, aes(x = Gender, y = Alignment)) +
+  geom_count() +
+  theme_minimal()
+
+## make contigency table
+t <- table(heroes_clean$Alignment, heroes_clean$Gender)
+
+## chi squared test
+chi <- chisq.test(t)
+chi$p.value
+chi$residuals
+corrplot(chi$residuals, is.cor = F)
